f628b7a419649df9b0072a7c415830609fef6324
bartp <bartek.paraf@gmail.com>
2023-07-26 22:05:48 +0200

    created tag relationships

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    public function categories(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function suppliers(): BelongsToMany
+    {
+        return $this->belongsToMany(Supplier::class);
+    }
 }
