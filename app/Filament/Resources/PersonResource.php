6d03d62dd3cc570a6bbb81cf43d3076402f3cac7
bartp <bartek.paraf@gmail.com>
2023-07-27 21:00:48 +0200

    created Person resource + pages

---
+++ app/Filament/Resources/PersonResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PersonResource\Pages;
+use App\Filament\Resources\PersonResource\RelationManagers;
+use App\Models\Person;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PersonResource extends Resource
+{
+    protected static ?string $model = Person::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    Select::make('type')
+                        ->options(['Main', 'Backup', 'Other'])
+                        ->required(),
+                    Select::make('gender')
+                        ->options(['Male', 'Female', 'Other'])
+                        ->required(),
+                    TextInput::make('first_name')
+                        ->required(),
+                    TextInput::make('last_name')
+                        ->required(),
+                    TextInput::make('position'),
+                    TextInput::make('phone1'),
+                    TextInput::make('phone2'),
+                    TextInput::make('email1'),
+                    TextInput::make('email2'),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('supplier_id')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('type')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('gender')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('first_name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('last_name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime('d.m.Y G:i', 'Europe/Berlin')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPeople::route('/'),
+            'create' => Pages\CreatePerson::route('/create'),
+            'edit' => Pages\EditPerson::route('/{record}/edit'),
+        ];
+    }
+}
