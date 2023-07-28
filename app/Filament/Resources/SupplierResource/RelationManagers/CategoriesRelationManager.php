7bd6ed28f86961a751b20430de99306f3f61c718
bartp <bartek.paraf@gmail.com>
2023-07-28 21:31:17 +0200

    Categories relation manager created

---
+++ app/Filament/Resources/SupplierResource/RelationManagers/CategoriesRelationManager.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Resources\SupplierResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoriesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'categories';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                ColorPicker::make('color')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('color'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\AttachAction::make()->preloadRecordSelect(),
+            ])
+            ->actions([
+                Tables\Actions\DetachAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([]);
+    }
+}
