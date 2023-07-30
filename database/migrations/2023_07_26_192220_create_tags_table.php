48f0bc51b40f1a57c6ad5cb0e4963b3ee7347645
bartp <bartek.paraf@gmail.com>
2023-07-30 15:32:20 +0200

    migrations updated

--- database/migrations/2023_07_26_192220_create_tags_table.php
+++ database/migrations/2023_07_26_192220_create_tags_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
