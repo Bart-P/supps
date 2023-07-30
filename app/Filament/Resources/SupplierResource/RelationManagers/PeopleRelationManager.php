b907307559c8c0ab3efa9430bbaf3440a4709012
bartp <bartek.paraf@gmail.com>
2023-07-30 16:51:06 +0200

    people relations manager created

---
+++ app/Filament/Resources/SupplierResource/RelationManagers/PeopleRelationManager.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources\SupplierResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PeopleRelationManager extends RelationManager
+{
+    protected static string $relationship = 'people';
+
+    protected static ?string $recordTitleAttribute = 'first_name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('type')
+                    ->options(['main' => 'Main', 'backup' => 'Backup', 'other' => 'Other'])
+                    ->required(),
+                Select::make('gender')
+                    ->options(['m' => 'Male', 'f' => 'Female', 'o' => 'Other'])
+                    ->required(),
+                TextInput::make('first_name')
+                    ->required(),
+                TextInput::make('last_name')
+                    ->required(),
+                TextInput::make('position'),
+                TextInput::make('phone1'),
+                TextInput::make('phone2'),
+                TextInput::make('email1'),
+                TextInput::make('email2'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('type')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('first_name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('last_name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('gender')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime('d.m.Y G:i', 'Europe/Berlin')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+}
