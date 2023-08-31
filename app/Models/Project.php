28112557019898ecb3bba5925feb3ef07a872292
bartp <bartek.paraf@gmail.com>
2023-08-31 20:55:07 +0200

    created item relation

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -13,4 +14,9 @@ class Project extends Model
         'name',
         'project_id',
     ];
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class);
+    }
 }
