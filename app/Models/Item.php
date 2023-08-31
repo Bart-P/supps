fbf1189c44d3fd40345fced796a4e98890f8d54c
bartp <bartek.paraf@gmail.com>
2023-08-31 20:54:57 +0200

    updated item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Item extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'category_id',
+        'product_id',
+        'name',
+        'description',
+        'quantities',
+    ];
+
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
