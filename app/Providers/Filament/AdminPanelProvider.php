635e7218acd42443dde26aaf98f0e135f5466e86
bartp <bartek.paraf@gmail.com>
2023-09-06 20:32:47 +0200

    LatestInquiries widget added to admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\NResource\Widgets\LatestInquiries;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -44,6 +45,7 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
+                LatestInquiries::class,
             ])
             ->middleware([
                 EncryptCookies::class,
