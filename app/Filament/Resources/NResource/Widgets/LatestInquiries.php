181ad5d9d6af382c6b3ded93f5f968617222058d
bartp <bartek.paraf@gmail.com>
2023-09-06 20:31:31 +0200

    latest Inquiries widget created

---
+++ app/Filament/Resources/NResource/Widgets/LatestInquiries.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\NResource\Widgets;
+
+use App\Models\Inquiry;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestInquiries extends BaseWidget
+{
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                Inquiry::query()
+            )
+            ->columns([
+                TextColumn::make('project_id'),
+                TextColumn::make('name')
+            ]);
+    }
+}
