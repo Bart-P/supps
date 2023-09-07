d2482cc493e1257fca794c673679574daa3354b9
bartp <bartek.paraf@gmail.com>
2023-09-07 18:34:07 +0200

    Inquiry resource pages created

---
+++ app/Filament/Resources/InquiryResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\InquiryResource\Pages;
+use App\Models\Inquiry;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class InquiryResource extends Resource
+{
+    protected static ?string $model = Inquiry::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('project_id'),
+                TextInput::make('name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('project_id'),
+                TextColumn::make('name'),
+                TextColumn::make('created_at'),
+                TextColumn::make('updated_at'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListInquiries::route('/'),
+            'create' => Pages\CreateInquiry::route('/create'),
+            'edit' => Pages\EditInquiry::route('/{record}/edit'),
+        ];
+    }
+}
