15a736694ca8de5edafa8dca608762f6d7af362f
bartp <bartek.paraf@gmail.com>
2023-09-10 17:11:55 +0200

    adjusted screen width for all pages

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -60,6 +60,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->maxContentWidth('screen-2xl');
     }
 }
