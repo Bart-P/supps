ac98e35d5aacde4a2db54c6da00d7d1f4ea02f4e
bartp <bartek.paraf@gmail.com>
2023-09-17 17:22:01 +0200

    field name changed

--- database/migrations/2023_08_31_174823_create_projects_table.php
+++ database/migrations/2023_08_31_174823_create_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('project_id');
+            $table->string('ext_id');
             $table->timestamps();
         });
     }
