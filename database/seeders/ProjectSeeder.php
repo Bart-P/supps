a596e388aeb29696f2fe5a963f5f468ed271d711
bartp <bartek.paraf@gmail.com>
2023-09-17 17:22:08 +0200

    field name changed

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -15,27 +15,27 @@ class ProjectSeeder extends Seeder
         Project::factory()->createMany([
             [
                 'name' => 'Tiny Love Display',
-                'project_id' => 'DORE-205',
+                'ext_id' => 'DORE-205',
             ],
             [
                 'name' => 'Geske Displays',
-                'project_id' => '3A-203',
+                'ext_id' => '3A-203',
             ],
             [
                 'name' => 'Recyclingbox Re-Order',
-                'project_id' => 'BRT-129',
+                'ext_id' => 'BRT-129',
             ],
             [
                 'name' => '3D-Logoaufsteller waldläufer',
-                'project_id' => 'LUGI-215',
+                'ext_id' => 'LUGI-215',
             ],
             [
                 'name' => 'Seat Stands',
-                'project_id' => 'DORE-163',
+                'ext_id' => 'DORE-163',
             ],
             [
                 'name' => 'Wandbedienplatz - Kom- Hartwick',
-                'project_id' => 'BÄCH-209',
+                'ext_id' => 'BÄCH-209',
             ],
         ]);
     }
