fcdf50a51108d5e094de25f33f8c1947fb9dba10
bartp <bartek.paraf@gmail.com>
2023-10-20 19:24:59 +0200

    inquiry factory created

--- database/factories/InquiryFactory.php
+++ database/factories/InquiryFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Inquiry;
+use App\Models\Item;
+use App\Models\Project;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +17,25 @@ class InquiryFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
     public function definition(): array
     {
+        $project_id = fake()->randomElement(Project::all()->pluck('id'));
+
         return [
-            //
+            'project_id' => $project_id,
+            'name' => fake()->sentence(random_int(1, 4)),
         ];
+    }
+
+    public function configure(): static
+    {
+        return $this->afterCreating(function (Inquiry $inquiry) {
+            $new_items = Item::factory(random_int(1, 4))->create([
+                'project_id' => $inquiry->project_id,
+            ]);
+
+            $inquiry->items()->attach($new_items);
+        });
     }
 }
