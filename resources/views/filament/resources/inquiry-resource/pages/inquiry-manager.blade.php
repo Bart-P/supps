31da2d1b2ecf34d542e97c26220699139a5e22fb
bartp <bartek.paraf@gmail.com>
2023-11-05 20:08:32 +0100

    using custom list-items component in view

--- resources/views/filament/resources/inquiry-resource/pages/inquiry-manager.blade.php
+++ resources/views/filament/resources/inquiry-resource/pages/inquiry-manager.blade.php
@@ -1,4 +1,6 @@
 <x-filament-panels::page>
+    {{-- TODO make some headings and sections --}}
     {{ $this->getInfolist }}
     {{ $this->table }}
+    <livewire:list-items :inquiry_id="$record->id" />
 </x-filament-panels::page>
