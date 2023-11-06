4747b8a72c7869a2fde0b83afbdfd51b9815e748
bartp <bartek.paraf@gmail.com>
2023-11-06 20:43:43 +0100

    updated latest inquiries on dashboard

--- app/Filament/Resources/NResource/Widgets/LatestInquiries.php
+++ app/Filament/Resources/NResource/Widgets/LatestInquiries.php
@@ -9,17 +9,24 @@ use Filament\Widgets\TableWidget as BaseWidget;
 
 class LatestInquiries extends BaseWidget
 {
-    protected int | string | array $columnSpan = 'full';
+    // protected int | string | array $columnSpan = 'full';
+    // TODO create latest projects Widget - same as here, just for projects
 
     public function table(Table $table): Table
     {
         return $table
+            ->paginated(false)
+            ->recordUrl(
+                fn (Inquiry $record) => route('filament.admin.resources.inquiries.manage', $record->id)
+            )
             ->query(
-                Inquiry::query()
+                Inquiry::query()->latest()->take(10)
             )
             ->columns([
-                TextColumn::make('project_id'),
-                TextColumn::make('name')
+                TextColumn::make('project.ext_id')
+                    ->label('Project'),
+                TextColumn::make('name'),
+                TextColumn::make('created_at'),
             ]);
     }
 }
